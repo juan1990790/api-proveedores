d218eeb29e38a8e96056d95527e9d603b1f6c931
juan1990790 <ju.opazoh@duocuc.cl>
2026-06-01 20:25:47 -0400

    Update ordenTransitoModel.java
    crear model

--- api-proveedores/src/main/java/cl/duoc/api_proveedores/model/ordenTransitoModel.java
+++ api-proveedores/src/main/java/cl/duoc/api_proveedores/model/ordenTransitoModel.java
@@ -1,4 +1,26 @@
 package cl.duoc.api_proveedores.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ordenes_transito")
 public class ordenTransitoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "id_fabricante")
+    private String idFabricante;
+
+    @Column(nullable = false, name = "cantidad_esperada")
+    private Integer cantidadEsperada;
+
+    @Column(nullable = false, name = "dias_llegada")
+    private Integer diasLlegada;
+
+    @Column(nullable = false, name = "tipo_envio")
+    private String tipoEnvio; // Ej: "Aéreo", "Marítimo"
 }
