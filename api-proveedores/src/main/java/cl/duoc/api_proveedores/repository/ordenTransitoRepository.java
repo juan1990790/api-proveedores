5f3e4b16b49e5bbbce4db02affc3a3448ab904a1
DaniloNunezN <dan.nunezn@duocuc.cl>
2026-06-26 21:43:21 -0400

    repository

--- api-proveedores/src/main/java/cl/duoc/api_proveedores/repository/ordenTransitoRepository.java
+++ api-proveedores/src/main/java/cl/duoc/api_proveedores/repository/ordenTransitoRepository.java
@@ -3,11 +3,14 @@ package cl.duoc.api_proveedores.repository;
 import cl.duoc.api_proveedores.model.ordenTransitoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 @Repository
 public interface ordenTransitoRepository extends JpaRepository<ordenTransitoModel, Long> {
-    
+
     Optional<ordenTransitoModel> findByIdFabricante(String idFabricante);
+
+    boolean existsByIdFabricante(String idFabricante);
 }
