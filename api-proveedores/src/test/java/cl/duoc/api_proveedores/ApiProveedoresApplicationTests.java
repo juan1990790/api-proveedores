e39a3aa0f11eb8ba774b223255ad2cc141d819d9
Byron Monsalve <by.monsalve@duocuc.cl>
2026-07-09 15:38:56 -0400

    Update ApiProveedoresApplicationTests.java

--- api-proveedores/src/test/java/cl/duoc/api_proveedores/ApiProveedoresApplicationTests.java
+++ api-proveedores/src/test/java/cl/duoc/api_proveedores/ApiProveedoresApplicationTests.java
@@ -3,7 +3,6 @@ package cl.duoc.api_proveedores;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
 class ApiProveedoresApplicationTests {
 
 	@Test
